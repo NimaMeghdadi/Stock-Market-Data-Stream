b6e720954afd5a6e772032c2e4f560354485ad29
Nima Meghdadi <meghdadi.nima@gmail.com>
2025-02-11 17:44:16 -0500

    docs(config): init config

---
+++ config/config.py
@@ -0,0 +1,4 @@
+ALPHA_ACCESS_KEY="WKT4ZA4LQGWWS6SF"
+MarketSTACK_ACCESS_KEY="53ae0c752ef1b8a546f3750cd95b01be"
+MAIN_PATH="~/Desktop/Stock-Market-Data-Stream/"
+LOG_PATH="~/Desktop/Stock-Market-Data-Stream/artifacts/logs/logs.log"
